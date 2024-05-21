6d64729cd893c857d5592af87461a463cbd169a3
marcosgabrielsr <marcosdevse@gmail.com>
2024-05-21 19:52:53 -0400

    Update buscaB function

--- b_three.c
+++ b_three.c
@@ -9,13 +9,13 @@
 
 //====== Definindo constantes
 #define D 2
-#define M 2*D
-#define MAX_SIZE_NAME 13
+#define TAMANHO_MAXIMO_NOME 13
 
 //====== Criando structs
 struct no{
-    int chaves[M];
-    struct no * filhos[M + 1];
+    int m;
+    int chaves[2 * D];
+    struct no * filhos[2 * D + 1];
 };
 
 //====== Declarando protótipos das funções
@@ -23,6 +23,8 @@ struct no{
 void menu();
 //- Função de busca em árvores B
 void buscaB(int x, struct no * r, struct no* pt, int* f, int* g);
+//- Função responsável por fazer a inserção de um novo elemento à árvore
+void insercao();
 
 int main() {
     struct no* raiz = NULL;
@@ -66,30 +68,39 @@ void menu() {
 
 //- Função de busca em árvores B
 void buscaB(int x, struct no * r, struct no* pt, int* f, int* g) {
-    struct no * p = r;
-    int i = 0;
-    *f = *g = 0;
+    struct no* p = r;
+    pt = NULL;
+    *f = 0;
 
-    while(p != NULL) {
-        pt = p;
-        i = 0;
+    //Enquanto o nó não for encontrado e enquanto ele não for uma folha
+    while(p != NULL){
+        //Inicializando i para marcar os índices, g para armazenar a posição em que o possível novo nó deveria ser incluso
+        //e armazenando o ponteiro da busca em pt
+        int i = 0;
         *g = 0;
-
-        while(i < M) {
-            if(x == p->chaves[i]) {
+        pt = p;
+        
+        //Enquanto i for menos que o número de chaves atual do nó
+        while(i < p->m){
+            //Caso x seja maior que a chave de índice i
+            if(x > p->chaves[i]){
+                i += 1;         
+                *g += 1;
+            //Caso x seja igual a chave de índice i
+            } else if(x == p->chaves){
+                //Alteramos p para NULL e *f para 1 => chave encontrada
+                p = NULL;
                 *f = 1;
-                return;
-            } else if(x > p->chaves[i]) {
-                if(i == M - 1){
-                    p = p->filhos[4];
-                    i = M;
-                }
-                else
-                    i += 1;
+            //Caso x seja menor que a chave de índice i
             } else {
+                //Vamos para o próximo nó de índice i e atualizamos o valor de i para sair do laço
                 p = p->filhos[i];
-                i = M;
+                i = p->m + 1;
             }
+            
+            //Caso i não seja menor que nenhuma das chaves já verificadas, enviamos p para o próximo nó
+            if(i == p->m)
+                p = p->filhos[i];
         }
     }
 }
